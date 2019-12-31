6ee898c1463fc714c51827eda7f9cd6bdc3a1e68
nuttuso14 <32212880+nuttuso14@users.noreply.github.com>
2019-12-31 21:31:57 +0800

    Happy New Year ^^

--- distribution_tw.cpp
+++ distribution_tw.cpp
@@ -50,7 +50,7 @@ int getWifiStatus(double p[],int size){
 int main(int argc, char *argv[]) 
 {
     
-    int Nsim = 10000;
+    int Nsim = 100000;
     double ets = 500;
     double et0 = 50;
     double et1 = 70;
@@ -61,9 +61,11 @@ int main(int argc, char *argv[])
     double lamda0 = 1/et0; 
     double gamma1 = 1/et1;
     double gamma2 = 1/et2;
+    double lamdaw = 1/etw;
 
 
-    double ts[Nsim];
+    //double ts[Nsim];
+    vector<double> ts; 
     map<int, double> countP; 
     map<int, double> countP2; 
 
@@ -71,7 +73,8 @@ int main(int argc, char *argv[])
 
     for(int i=0;i<Nsim;i++)
     {
-        ts[i] = GenExpo(ets);
+        //ts[i] = GenExpo(ets);
+        ts.push_back(GenExpo(ets)); 
     }
 
     // do simulation
@@ -80,10 +83,12 @@ int main(int argc, char *argv[])
     wifistatus = getWifiStatus(plist,3);
     //cout << "WiFi status: " << wifistatus <<endl;
     double tnext = -1;
-
+    double countT1 = 0;
+    double countT2 = 0;
     for(int i=0;i<Nsim;i++)
     {
         double tsi = ts[i];
+        //cout << "ts:" << tsi << endl;
         do
         {
 
@@ -145,7 +150,7 @@ int main(int argc, char *argv[])
                             //cout << "gen t1" << endl;
                             t1 = GenExpo(et1);
                         }
-
+                       // cout << "t1:"<<t1<<endl;
                         tsi -=t1;
                         if(tsi<0)
                         {
@@ -171,6 +176,7 @@ int main(int argc, char *argv[])
                             use_laststatus = 0;
                         }
                         wificount++;
+                       
                     }
                     else
                     {
@@ -187,7 +193,7 @@ int main(int argc, char *argv[])
                             //cout << "this loop" <<endl;
                             t2 = GenExpo(et2);
                         }
-
+                        //cout << "t2:" <<t2<<endl;
                         tsi -=t2;
                         if(tsi<0)
                         {
@@ -213,63 +219,138 @@ int main(int argc, char *argv[])
                             use_laststatus = 0;
                         }
                         wificount2++;
+                      
                     }
                     //cout << wifistatus << "," << use_laststatus << endl;
                }while(wifistatus!=0 &&  use_laststatus !=1);
 
                 if(countP.find(wificount)==countP.end()) // if there is no P[N=n]
                 {
-                 countP.insert(make_pair(wificount,1));
+                    countP.insert(make_pair(wificount,1));
+                    countT1++;
                 }
                 else
                 {
                     int wcount = countP[wificount];
                     countP[wificount] = wcount+1;
+                    countT1++;           
                 } 
 
                 if(countP2.find(wificount2)==countP2.end()) // if there is no P[N=n]
                 {
-                 countP2.insert(make_pair(wificount2,1));
+                    countP2.insert(make_pair(wificount2,1));
+                    countT2++;
                 }
                 else
                 {
                     int wcount = countP2[wificount2];
                     countP2[wificount2] = wcount+1;
+                    countT2++;
                 } 
             }
   
         }while(tsi>0);
     }
     cout << "============ Report ============" <<endl;
+    cout << "Total State 1 :" << countT1 << endl;
+    cout << "Total State 2 :" << countT2 << endl;
     cout << "========= State 1 ==============" <<endl;
-    int n_sum = 0;
+    /*int n_sum = 0;
     for (map<int, double>::iterator it = countP.begin(); it != countP.end(); ++it) 
     {
         int n = it->first;
         n_sum+=(it->second);
         //cout << "P[N="<<n<<"]="<<(it->second)<<endl;
-    }
-    cout << "n_sum:"<<n_sum<<endl;
+    }*/
+    //cout << "n_sum:"<<n_sum<<endl;
+   /* for (map<int, double>::iterator it = countP.begin(); it != countP.end(); ++it) 
+    {
+        int n = it->first;
+        //cout << "P[N="<<n<<"]="<<(it->second)/(double)countT1<<endl;
+    }*/
+
+    double summ =0;
+    cout <<setw(5) << "P[N=n]| " <<setw(15)<<" Simulation |" << setw(15) << " Math Analyisis |" <<endl;
     for (map<int, double>::iterator it = countP.begin(); it != countP.end(); ++it) 
     {
         int n = it->first;
-        cout << "P[N="<<n<<"]="<<(it->second)/(double)n_sum<<endl;
+        double q1 = gamma2/(gamma1+gamma2);
+        double q2 = gamma1/(gamma1+gamma2);
+        double F1 = gamma1/(gamma1+lamdaw+mus);
+        double F2 = gamma2/(gamma2+lamdaw+mus);
+        double P1 =(pow(F1*F2,n-1)*(1-F1))*q1;
+        double P2 =(pow(F1,n)*pow(F2,n-1)*(1-F2))*q1;
+        double P3 =((1-q1)*pow(F2,n))*pow(F1,n-1)*((1-F1));
+        double P4 =((1-q1)*(pow(F1*F2,n))*((1-F2)));
+        double p = 0;
+
+        if(n==0)
+        {
+            p = P4;
+        }
+        else{
+            p = P1+P2+P3+P4; 
+        }
+        cout << fixed;
+        string col1 = "P[" + to_string(n) + "]";
+        double psim = (it->second)/countT1;
+        cout << setw(5) <<left << col1 << setprecision(4) << setw(15)<< right  << psim << setw(15) << setprecision(4)<< right << p <<endl;
+        summ +=p;
+
+        /*content += to_string(Nsim)+ "," + to_string(ets) + "," + to_string(et0) + "," + to_string(et1) + ","+ to_string(n) \
+                  + "," +  to_string(psim) + "," +  to_string(p) + "\n";*/
+
     }
+    cout << "psum:"<< summ <<endl;
 
     cout << "========= State 2 ==============" <<endl;
-    int n_sum2 = 0;
+    /*int n_sum2 = 0;
     for (map<int, double>::iterator it = countP2.begin(); it != countP2.end(); ++it) 
     {
         int n = it->first;
         n_sum2+=(it->second);
        // cout << "P[N="<<n<<"]="<<(it->second)<<endl;
     }
-    cout << "n_sum2:"<<n_sum2<<endl;
-    for (map<int, double>::iterator it = countP2.begin(); it != countP2.end(); ++it) 
+    cout << "n_sum2:"<<n_sum2<<endl;*/
+    /*for (map<int, double>::iterator it = countP2.begin(); it != countP2.end(); ++it) 
     {
         int n = it->first;
-        cout << "P[N="<<n<<"]="<<(it->second)/(double)n_sum2<<endl;
+        cout << "P[N="<<n<<"]="<<(it->second)/(double)countT2<<endl;
+    }*/
+    double summ2 =0;
+    cout <<setw(5) << "P[N=n]| " <<setw(15)<<" Simulation |" << setw(15) << " Math Analyisis |" <<endl;
+     for (map<int, double>::iterator it = countP2.begin(); it != countP2.end(); ++it) 
+    {
+        int n = it->first;
+        double q1 = gamma2/(gamma1+gamma2);
+        double q2 = gamma1/(gamma1+gamma2);
+        double F1 = gamma1/(gamma1+lamdaw+mus);
+        double F2 = gamma2/(gamma2+lamdaw+mus);
+        double P1 =(pow(F1*F2,n-1)*(1-F2))*q2;
+        double P2 =(pow(F2,n)*pow(F1,n-1)*(1-F1))*q2;
+        double P3 =((1-q2)*pow(F1,n))*pow(F2,n-1)*((1-F2));
+        double P4 =((1-q2)*(pow(F1*F2,n))*((1-F1)));
+        double p = 0;
+
+        if(n==0)
+        {
+            p = P4;
+        }
+        else{
+            p = P1+P2+P3+P4; 
+        }
+        cout << fixed;
+        string col1 = "P[" + to_string(n) + "]";
+        double psim = (it->second)/countT2;
+        cout << setw(5) <<left << col1 << setprecision(4) << setw(15)<< right  << psim << setw(15) << setprecision(4)<< right << p <<endl;
+        summ2 +=p;
+
+        /*content += to_string(Nsim)+ "," + to_string(ets) + "," + to_string(et0) + "," + to_string(et1) + ","+ to_string(n) \
+                  + "," +  to_string(psim) + "," +  to_string(p) + "\n";*/
+
     }
+    cout << "psum:"<< summ2 <<endl;
+    
 
     return 0;
 
